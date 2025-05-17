631c1d951b8910ac2e286901eb5dba35e465d8b1
eh-munna <emran.h.munna@gmail.com>
2025-05-17 18:30:46 +0200

    3.12

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,49 @@
-import Accordion from './components/Accordion';
+import { useState } from 'react';
+import Tasks from './components/Tasks';
+import { initialTasks } from './data/initialTasks';
 
-function App() {
+export default function App() {
+  const [tasks, setTasks] = useState(initialTasks);
+
+  const handleAddTask = (newTask) => {
+    setTasks([
+      ...tasks,
+      {
+        id: tasks.length + 1,
+        text: newTask,
+        done: false,
+      },
+    ]);
+  };
+
+  const handleEditTask = (taskId, task) => {
+    const updatedTasks = tasks.map((t) => {
+      if (t.id === task.taskId) {
+        return task;
+      } else {
+        return t;
+      }
+    });
+    setTasks(updatedTasks);
+  };
+
+  const handleDeleteTask = (taskId) => {
+    setTasks(tasks.filter((task) => task.id !== taskId));
+  };
+
+  console.log('Current tasks:', tasks);
+
   return (
     <>
       {/* <div className="text-red-400 flex flex-col space-y-4 w-1/2 gap-6 mx-auto justify-center items-center min-h-[75vh]"> */}
       <div className="text-red-400 flex flex-col">
-        <Accordion />
+        <Tasks
+          tasks={tasks}
+          onAddTask={handleAddTask}
+          onEditTask={handleEditTask}
+          onDeleteTask={handleDeleteTask}
+        />
       </div>
     </>
   );
 }
-
-export default App;
